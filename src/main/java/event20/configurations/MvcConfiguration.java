6bb0a9ef64eefd23d87b7500f7915ed756a9662f
Nikolay A Rangelov <nikolay.a.rangelov@mail.bg>
2017-05-31 19:09:05 +0300

    Spring Security added with in-memory authentication

---
+++ src/main/java/event20/configurations/MvcConfiguration.java
@@ -0,0 +1,21 @@
+package event20.configurations;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by nik on 5/31/17.
+ */
+@Configuration
+public class MvcConfiguration extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+
+}
